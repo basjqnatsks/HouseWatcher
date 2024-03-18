ee402986db21b426cc62a15b6fb953ab4c853b73
basjqnatsks <mjhfraud@gmail.com>
2024-03-17 19:51:18 -0700

    added nasdaq api key

--- nasdaq_test.py
+++ nasdaq_test.py
@@ -1,11 +1,14 @@
 import nasdaqdatalink
-
+from read import read
 class Nasdaq:
     def __init__(self) -> None:
-        pass
+        self.key = self.get_api_key()
 
 
 
+    @staticmethod
+    def get_api_key() -> str:
+        return read('nasdaq.api')
 
 
 Nasdaq()
