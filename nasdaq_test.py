c40d99ed7020bc8848c550e6ff8fb03252527a14
basjqnatsks <mjhfraud@gmail.com>
2024-03-17 20:04:42 -0700

    Not sure is NASDAQ is an option

--- nasdaq_test.py
+++ nasdaq_test.py
@@ -3,9 +3,11 @@ from read import read
 class Nasdaq:
     def __init__(self) -> None:
         self.key = self.get_api_key()
-
-
-
+        nasdaqdatalink.read_key(filename="nasdaq.api")
+        data = nasdaqdatalink.get('NSE/OIL')
+        #data = nasdaqdatalink.get_table('ZACKS/FC', ticker='AAPL')
+    
+        print(data)
     @staticmethod
     def get_api_key() -> str:
         return read('nasdaq.api')
